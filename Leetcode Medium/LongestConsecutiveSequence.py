cb6505cd51b5cdb0357f8a66f0dcbab73bf477af
d3h1 <denicabaravdic@gmail.com>
2023-07-21 15:38:06 -0700

    doing consecutive string with notes

--- Leetcode Medium/LongestConsecutiveSequence.py
+++ Leetcode Medium/LongestConsecutiveSequence.py
@@ -5,10 +5,14 @@ class Solution:
         numSet = set(nums)
         longest = 0
         
+        # We have to run through each number in the nums list 
         for n in nums:
+            # This is checking the left of that number
             if n - 1 not in numSet:
                 length = 1
+                # SO if there is no left, we start with that one to make a sequence and continue forwards until we do not have a consecutive length 
                 while (n + length) in numSet:
                     length += 1
+                # Getting the longest from the length
                 longest = max(length, longest)
         return longest
