091a06b88ad0d78e23e92809bf21a2298a191923
d3h1 <denicabaravdic@gmail.com>
2023-08-08 15:59:57 -0700

    finishing container with most water, will come back

--- Leetcode Medium/ContainerWithMostWater.py
+++ Leetcode Medium/ContainerWithMostWater.py
@@ -1,3 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        pass
+        # Since we are finding area of most water, we need to init a res which will be the max area 
+        res = 0
+        
+        # Declare your pointers because we have to from left to right to understand 
+        l, r = 0, len(height - 1)
+        
+        while l < r:
+            area = min(height[r], height[l]) * (r - l)
+            res = max(res, area)
+            
+            if height[r] > height[l]:
+                l += 1
+            
+            elif height[r] <= height[l]:
+                r -= 1
+            
+            
+        return res
