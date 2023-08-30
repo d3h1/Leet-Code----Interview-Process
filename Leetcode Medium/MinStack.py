b767bc3801a2c280507b60eecd68409ac659ad67
denicabaravdic <denicabaravdic@gmail.com>
2023-08-29 19:27:23 -0700

    creating the top functionality

--- Leetcode Medium/MinStack.py
+++ Leetcode Medium/MinStack.py
@@ -4,3 +4,19 @@ class MinStack:
         # We create empty stacks for both (a regular stack and a min stack that stacks value to)
         self.stack = []
         self.minStack = []
+        
+    # Create the push functionality
+    def push(self, val: int) -> None:
+           # First stack is easily appendable
+           self.stack.append(val)
+           # Min stack needs a checker as val being appended can be cur val or top of stack if stack is non-empty
+           val = min(val, self.minStack[-1] if self.stack else val)
+           self.minStack.append(val)
+
+    # Create the pop functionality
+    def pop(self) -> None:
+        # Popping is pretty straight forward -- just use pythons built in pop functionaility
+        self.stack.pop()
+        self.minStack.pop()
+    
+    # Create the 
