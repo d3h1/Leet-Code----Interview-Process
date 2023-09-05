3de44e329b4d381b56e17c0199ec9503d5fec6dd
d3h1 <denicabaravdic@gmail.com>
2023-09-05 10:18:02 -0700

    some more notes

--- Leetcode Medium/CarFleet.py
+++ Leetcode Medium/CarFleet.py
@@ -5,7 +5,7 @@
 class Solution:
     def CarFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pair = [(p,s) for p, s in zip(position, speed)]
-        pair.sort(reverse=True)
+        pair.sort(reverse=True) # This is the reasoning behind reversing it. The values that are at bottom will be on top so we can add to the stack right to left
         
         stack = []
         
@@ -14,3 +14,6 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop() # we pop values that are greater than a length of 2 as well as less than the value before it
         return len(stack)
+    
+    #! TIME COMPLEXITY : O(nlogn) -- due to sorting
+    #! SPACE COMPLEXITY : O(n)
